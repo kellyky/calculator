da6795093eddb53a0c2e4b5e2f4acb98a297a146
Kelly Young <kkristineyoung@gmail.com>
2021-01-16 10:38:55 -0500

    Adds logic to handle user input

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,26 @@
-// Addition
-function add(number1, number2) {
+// Business logic:
+const add = function(number1, number2) {
   return number1 + number2;
 }
 
-// Subtraction
-function subtract(number1, number2){
+const subtract = function(number1, number2){
   return number1 - number2;
 }
 
-// Multiplication
-function multiply(number1, number2){
+const multiply = function(number1, number2){
   return number1 * number2;
 }
 
-// Division
-function divide(number1, number2){
+const divide = function(number1, number2){
   return number1 / number2;
 }
 
-// Assigns values to number1 and number2 based on user input
-const number1 = parseInt(prompt("Enter a number:"));
-const number2 = parseInt(prompt("Enter another number:"));
-
-// Assigns value to result based on the function called
-const result = multiply(number1, number2);
-
-// Alert to show the user the result
-alert(result);
+$(document).ready(function() {
+  $("form#add").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#add1").val());
+    const number2 = parseInt($("#add2").val());
+    const result = add(number1, number2);
+    $("#output").text(result);
+  });
+});
