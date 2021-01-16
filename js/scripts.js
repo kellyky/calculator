86dc44b898be5ebc9f9fa4181054ce04295f0e9a
Kelly Young <kkristineyoung@gmail.com>
2021-01-16 11:23:22 -0500

    Adds interface for multiplication and division

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,7 @@ const divide = function(number1, number2){
 
 // User Interface Logic 
 $(document).ready(function() {
+  // Addition
   $("form#add").submit(function(event) {
     event.preventDefault();
     const number1 = parseInt($("#add1").val());
@@ -24,11 +25,28 @@ $(document).ready(function() {
     const result = add(number1, number2);
     $("#output-addition").text(result);
   });
+  // Subtraction
   $("form#subtract").submit(function(event) {
     event.preventDefault();
     const number1 = parseInt($("#subtract1").val());
     const number2 = parseInt($("#subtract2").val());
     const result = subtract(number1, number2);
     $("#output-subtraction").text(result);
+  });
+  // Multiplication
+  $("form#multiply").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#multiply1").val());
+    const number2 = parseInt($("#multiply2").val());
+    const result = multiply(number1, number2);
+    $("#output-multiplication").text(result);
+  });
+  // Division
+  $("form#divide").submit(function(event) {
+    event.preventDefault();
+    const number1 = parseInt($("#divide1").val());
+    const number2 = parseInt($("#divide2").val());
+    const result = divide(number1, number2);
+    $("#output-division").text(result);
   });
 });
